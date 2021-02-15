3d326964c3b046b80bb65fe039553dc30da67162
Antonio Mallia <me@antoniomallia.it>
2021-02-15 08:26:10 -0500

    Some changes

--- src/model.py
+++ src/model.py
@@ -1,83 +1,165 @@
-import string
 import torch
 import torch.nn as nn
 
-from transformers import BertPreTrainedModel, BertModel, BertTokenizer
+from nltk.stem import PorterStemmer
+from random import sample, shuffle, randint
+
+from itertools import accumulate
+from transformers import *
+import re
 from src.parameters import DEVICE
+from src.utils2 import cleanQ, cleanD
+
+stem = PorterStemmer().stem
+
+MAX_LENGTH = 160
 
 
-class ColBERT(BertPreTrainedModel):
-    def __init__(self, config, query_maxlen, doc_maxlen, dim=128, similarity_metric='cosine'):
-        super(ColBERT, self).__init__(config)
+def unique(seq):
+    seen = set()
+    return [x for x in seq if not (x in seen or seen.add(x))]
 
-        self.query_maxlen = query_maxlen
-        self.doc_maxlen = doc_maxlen
-        self.similarity_metric = similarity_metric
+
+class MultiBERT(BertPreTrainedModel):
+    def __init__(self, config):
+        super(MultiBERT, self).__init__(config)
 
         self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-        self.skiplist = {w: True for w in string.punctuation}
+        self.regex_drop_char = re.compile('[^a-z0-9\s]+')
+        self.regex_multi_space = re.compile('\s+')
 
         self.bert = BertModel(config)
-        self.linear = nn.Linear(config.hidden_size, dim, bias=False)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        self.pre_classifier = nn.Linear(config.hidden_size, config.hidden_size)
+        self.classifier = nn.Linear(config.hidden_size, 1)
 
         self.init_weights()
 
+    def myforward(self, input_ids, attention_mask, token_type_ids):
+        extended_attention_mask = attention_mask.unsqueeze(1).unsqueeze(2)
+        extended_attention_mask = extended_attention_mask.to(dtype=next(self.parameters()).dtype)  # fp16 compatibility
+        extended_attention_mask = (1.0 - extended_attention_mask) * -10000.0
+        head_mask = [None] * self.config.num_hidden_layers
+
+        embedding_output = self.bert.embeddings(input_ids, position_ids=None, token_type_ids=token_type_ids)
+        encoder_outputs = self.encoder(embedding_output, extended_attention_mask, head_mask=head_mask)
+        sequence_output = encoder_outputs[0]
+
+        return (sequence_output,)
+
+    def encoder(self, hidden_states, attention_mask, head_mask):
+        for i, layer_module in enumerate(self.bert.encoder.layer):
+            if i == 7:
+                break
+            layer_outputs = layer_module(hidden_states, attention_mask, head_mask[i])
+            hidden_states = layer_outputs[0]
+
+        return (hidden_states,)
+
+
+    def convert_example(self, d, max_seq_length):
+        max_length = min(MAX_LENGTH, max_seq_length)
+        inputs = self.tokenizer.encode_plus(d, add_special_tokens=True, max_length=max_length, truncation=True)
+
+        padding_length = max_length - len(inputs["input_ids"])
+        attention_mask = ([1] * len(inputs["input_ids"])) + ([0] * padding_length)
+        input_ids = inputs["input_ids"] + ([0] * padding_length)
+        token_type_ids = inputs["token_type_ids"] + ([0] * padding_length)
+
+        return {'input_ids': input_ids, 'attention_mask': attention_mask, 'token_type_ids': token_type_ids}
+
+    def tokenize(self, q, d):
+        query_tokens = list(set(cleanQ(q).strip().split()))  # [:10]
+
+        content = cleanD(d).strip()
+        doc_tokens = content.split()
+
+        # NOTE: The following line accounts for CLS!
+        tokenized = self.tokenizer.tokenize(content)
+        word_indexes = list(accumulate([-1] + tokenized, lambda a, b: a + int(not b.startswith('##'))))
+        match_indexes = list(set([doc_tokens.index(t) for t in query_tokens if t in doc_tokens]))
+        term_indexes = [word_indexes.index(idx) for idx in match_indexes]
+
+        a = [idx for i, idx in enumerate(match_indexes) if term_indexes[i] < MAX_LENGTH]
+        b = [idx for idx in term_indexes if idx < MAX_LENGTH]
+
+        return content, tokenized, a, b, len(word_indexes) + 2
+
     def forward(self, Q, D):
-        return self.score(self.query(Q), self.doc(D))
+        bsize = len(Q)
+        pairs = []
+        X, pfx_sum, pfx_sumX = [], [], []
+        total_size, total_sizeX, max_seq_length = 0, 0, 0
 
-    def query(self, queries):
-        queries = [["[unused0]"] + self._tokenize(q) for q in queries]
+        doc_partials = []
+        pre_pairs = []
 
-        input_ids, attention_mask = zip(*[self._encode(x, self.query_maxlen) for x in queries])
-        input_ids, attention_mask = self._tensorize(input_ids), self._tensorize(attention_mask)
+        for q, d in zip(Q, D):
+            tokens, tokenized, term_idxs, token_idxs, seq_length = self.tokenize(q, d)
+            max_seq_length = max(max_seq_length, seq_length)
 
-        Q = self.bert(input_ids, attention_mask=attention_mask)[0]
-        Q = self.linear(Q)
+            pfx_sumX.append(total_sizeX)
+            total_sizeX += len(term_idxs)
 
-        return torch.nn.functional.normalize(Q, p=2, dim=2)
+            tokens_split = tokens.split()
 
-    def doc(self, docs, return_mask=False):
-        docs = [["[unused1]"] + self._tokenize(d)[:self.doc_maxlen-3] for d in docs]
+            doc_partials.append([(total_size + idx, tokens_split[i]) for idx, i in enumerate(term_idxs)])
+            total_size += len(doc_partials[-1])
+            pfx_sum.append(total_size)
 
-        lengths = [len(d)+2 for d in docs]  # +2 for [CLS], [SEP]
-        d_max_length = max(lengths)
+            pre_pairs.append((tokenized, token_idxs))
 
-        input_ids, attention_mask = zip(*[self._encode(x, d_max_length) for x in docs])
-        input_ids, attention_mask = self._tensorize(input_ids), self._tensorize(attention_mask)
+        if max_seq_length % 10 == 0:
+            print("#>>>   max_seq_length = ", max_seq_length)
 
-        D = self.bert(input_ids, attention_mask=attention_mask)[0]
-        D = self.linear(D)
+        for tokenized, token_idxs in pre_pairs:
+            pairs.append(self.convert_example(tokenized, max_seq_length))
+            X.append(token_idxs)
 
-        # [CLS] .. d ... [SEP] [PAD] ... [PAD]
-        mask = [[1] + [x not in self.skiplist for x in d] + [1] + [0] * (d_max_length - length)
-                for d, length in zip(docs, lengths)]
+        input_ids = torch.tensor([f['input_ids'] for f in pairs], dtype=torch.long).to(DEVICE)
+        attention_mask = torch.tensor([f['attention_mask'] for f in pairs], dtype=torch.long).to(DEVICE)
+        token_type_ids = torch.tensor([f['token_type_ids'] for f in pairs], dtype=torch.long).to(DEVICE)
 
-        D = D * torch.tensor(mask, device=DEVICE, dtype=torch.float32).unsqueeze(2)
-        D = torch.nn.functional.normalize(D, p=2, dim=2)
+        outputs = self.myforward(input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids)
 
-        return (D, mask) if return_mask else D
+        hidden_state = outputs[0]
 
-    def score(self, Q, D):
-        if self.similarity_metric == 'cosine':
-            return (Q @ D.permute(0, 2, 1)).max(2).values.sum(1)
-        
-        assert self.similarity_metric == 'l2'
-        return (-1.0 * ((Q.unsqueeze(2) - D.unsqueeze(1))**2).sum(-1)).max(-1).values.sum(-1)
+        def one(i):
+            if len(X[i]) > 0:
+                l = [hidden_state[i, j] for j in X[i]]  # + [mismatch_scores[i, j] for j in all_mismatches[i]]
+                return torch.stack(l)
+            return torch.tensor([]).to(DEVICE)
 
-    def _tokenize(self, text):
-        if type(text) == list:
-            return text
+        pooled_output = torch.cat([one(i) for i in range(bsize)])
 
-        return self.tokenizer.tokenize(text)
+        bsize = len(pooled_output)
 
-    def _encode(self, x, max_length):
-        input_ids = self.tokenizer.encode(x, add_special_tokens=True, max_length=max_length)
+        if bsize == 0:
+            term_scores = []
+            for doc in doc_partials:
+                term_scores.append([])
+                for (idx, term) in doc:
+                    term_scores[-1].append((term, 0.0))
 
-        padding_length = max_length - len(input_ids)
-        attention_mask = [1] * len(input_ids) + [0] * padding_length
-        input_ids = input_ids + [103] * padding_length
+            return torch.tensor([[0.0]] * len(Q)).to(DEVICE), term_scores
 
-        return input_ids, attention_mask
+        pooled_output = self.pre_classifier(pooled_output)
+        pooled_output = nn.ReLU()(pooled_output)
+        pooled_output = self.dropout(pooled_output)
 
-    def _tensorize(self, l):
-        return torch.tensor(l, dtype=torch.long, device=DEVICE)
+        y_score = self.classifier(pooled_output)
+        y_score = torch.nn.functional.relu(y_score)
+
+        x = torch.arange(bsize).expand(len(pfx_sum), bsize) < torch.tensor(pfx_sum).unsqueeze(1)
+        y = torch.arange(bsize).expand(len(pfx_sum), bsize) >= torch.tensor([0] + pfx_sum[:-1]).unsqueeze(1)
+        mask = (x & y).to(DEVICE)
+
+        y_scorex = list(y_score.cpu())
+        term_scores = []
+        for doc in doc_partials:
+            term_scores.append([])
+            for (idx, term) in doc:
+                term_scores[-1].append((term, y_scorex[idx]))
+
+        return (mask.type(torch.float32) @ y_score), term_scores #, ordered_terms #, num_exceeding_fifth
+
--- src/training/data_reader.py
+++ src/training/data_reader.py
@@ -8,9 +8,9 @@ from transformers import AdamW
 
 from src.parameters import DEVICE, SAVED_CHECKPOINTS
 
-from src.model import ColBERT
+from src.model import MultiBERT
 from src.utils import print_message, save_checkpoint
-
+import re
 
 class TrainReader:
     def __init__(self, data_file):
@@ -30,11 +30,7 @@ def manage_checkpoints(colbert, optimizer, batch_idx):
 
 
 def train(args):
-    colbert = ColBERT.from_pretrained('bert-base-uncased',
-                                      query_maxlen=args.query_maxlen,
-                                      doc_maxlen=args.doc_maxlen,
-                                      dim=args.dim,
-                                      similarity_metric=args.similarity)
+    colbert = MultiBERT.from_pretrained('bert-base-uncased')
     colbert = colbert.to(DEVICE)
     colbert.train()
 
@@ -56,15 +52,15 @@ def train(args):
             B = Batch[B_idx * size: (B_idx+1) * size]
             Q, D1, D2 = zip(*B)
 
-            colbert_out = colbert(Q + Q, D1 + D2)
+            colbert_out, _ = colbert(Q + Q, D1 + D2)
             colbert_out1, colbert_out2 = colbert_out[:len(Q)], colbert_out[len(Q):]
 
             out = torch.stack((colbert_out1, colbert_out2), dim=-1)
 
             positive_score, negative_score = round(colbert_out1.mean().item(), 2), round(colbert_out2.mean().item(), 2)
             print("#>>>   ", positive_score, negative_score, '\t\t|\t\t', positive_score - negative_score)
-
-            loss = criterion(out, labels[:out.size(0)])
+            print(out.size())
+            loss = criterion(out.squeeze(1), labels[:out.size(0)])
             loss = loss / args.accumsteps
             loss.backward()
 
