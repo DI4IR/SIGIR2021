a6b1ee4efaba7d0de75501f2f05a4b9353cdb673
Antonio Mallia <me@antoniomallia.it>
2021-10-12 00:21:14 +0200

    Update quantize.py

--- scripts/quantize.py
+++ scripts/quantize.py
@@ -19,8 +19,13 @@ def find_max_value(input_filename):
                     max_val = max(max_val, float(score))
     return max_val
 
-def process(input_filename, output_filename):
-    max_val = find_max_value(input_filename)
+def process(input_filename, output_filename, max_val_provided = None):
+    if max_val_provided:
+        max_val= float(max_val_provided)
+        print("We will use {} as max val".format(max_val_provided))
+    else:
+        max_val = find_max_value(input_filename)
+        print("Max Val is: {}".format(max_val))
     scale = (1<<QUANTIZATION_BITS)/max_val
     with open(input_filename) as input_file, open(output_filename, "w+") as output_file:
         for docid, line in tqdm(enumerate(input_file)):
@@ -37,9 +42,9 @@ def main():
     parser = ArgumentParser(description='Quantize a DeepImpact collection.')
     parser.add_argument('--input', dest='input', required=True)
     parser.add_argument('--output', dest='output', required=True)
+    parser.add_argument('--max', dest='max', required=False, default=None)
     args = parser.parse_args()
-    process(args.input, args.output)
+    process(args.input, args.output, args.max)
 
 if __name__ == "__main__":
     main()
-
